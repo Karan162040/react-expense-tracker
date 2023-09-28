c48385a0f90018c51c3f9006acd349b8eaec9aec
Karan162040 <kumar.karan.162040@gmail.com>
2023-09-28 17:14:30 +0530

    Added change title & amount button with state

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import "./ExpenseDetails.css";
 const ExpenseDetails = (props) => {
-  const title = props.title;
   const location = props.location;
-  const amount = props.amount;
-  const clickHandler = () => {
-    console.log("clicked!!!");
+
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+  const clickHandler1 = () => {
+    setTitle("Updated");
+  };
+  const clickHandler2 = () => {
+    setAmount(100);
   };
   return (
     <div className="expense-item__description">
       <h2>{title}</h2>
       <h2>{location}</h2>
       <div className="expense-item__price">Rs {amount}</div>
-      <button onClick={clickHandler}>Delete</button>
+      <button onClick={clickHandler2}>Change Amount</button>
+      <button onClick={clickHandler1}>Change Title</button>
     </div>
   );
 };
